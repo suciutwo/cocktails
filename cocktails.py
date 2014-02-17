d3a0b0523c86545296094fb06b77f8c6257d61cf
2plus2make5 <2plus2make5@gmail.com>
2014-02-17 15:26:11 -0800

    added recipe parsing code

--- cocktails.py
+++ cocktails.py
@@ -1,8 +1,11 @@
+from bs4 import BeautifulSoup
 import urllib2
-from BeautifulSoup import BeautifulSoup
 from threading import Thread
 import pickle
 import Queue
+def scanRecipe():
+	soup=BeautifulSoup(urlopen(url))
+	l=soup.findAll('li', {'class':'ingredient'})
 
 COCKTAILS_ON_DATABASE = 4758
 INTERNET_DB_URL = 'http://www.cocktaildb.com/recipe_detail?id='
@@ -15,19 +18,22 @@ def downloadAllPages():
 		urls.append(INTERNET_DB_URL+str(i))
 
 	results = [None]*len(urls)
-	
+
 	def worker():
+		n_read = 0
 		while True:
 			i, url = q.get()
 			try: 
 				results[i] = (urllib2.urlopen(url).read())
+				print i, n_read
+				n_read+=1
 			except Exception as e:
 				print "failed on " + str(i)
 				print e
 
 
 			q.task_done()
-			
+
 	q = Queue.Queue()
 	for i in range(WORKERS):
 		t = Thread(target=worker)
@@ -41,30 +47,30 @@ def downloadAllPages():
 
 	print len(results)
 	pickle.dump(results, open("savedCocktails", 'wb'))
-
-
-
-
-def loadAllPages():
-	return pickle.load(open("savedCocktails", 'rb'))
-
-
+def processRecipes():
+	d = pickle.load(open("savedCocktails"))
+	all_recipes = []
+	for i, d_i in enumerate(d):
+		soup=BeautifulSoup(d_i)
+		l=soup.findAll('div', {'class':'recipeMeasure'})
+		print 'Recipe %i' % (i)
+		recipe = []
+		try:
+			for ingredient in l:
+				comps = str(ingredient).split('>')
+				amount = comps[1].split('<')[0].strip()
+				ingredient = comps[2].split('<')[0].strip()
+				recipe.append([ingredient, amount])
+			all_recipes.append(recipe)
+		except:
+			continue
+	pickle.dump(all_recipes, open("cleanedRecipes", 'wb'))	
 if __name__ == '__main__':
-	data = loadAllPages()
-	for page in data:
-		soup = BeautifulSoup(page)
-	#	title = soup.title.string.split(' | ')[1]
-	#	print title
-		for thing in soup:
-			print thing
-			break
-		
-		print soup
-		ingredients = soup.find_all(class_="recipeMeasure")
-		#print len(ingredients)
-		#print ingredients
-		break
-	
+	download = 0
+	if download:
+		downloadAllPages()
+	else:
+		processRecipes()	
 
 
 
