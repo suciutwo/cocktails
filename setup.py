bd040ed6e41a745180142fc2d71ea7174d29a852
suciutwo <andrewsuciu@gmail.com>
2014-11-12 13:42:47 -0800

    Fooling around.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(name='cocktails',
       version='0.2',
       description='Cocktail Generation and Analysis',
       author='Emma Pierson and Andrew Suciu',
       author_email='suciu@cs.stanford.edu',
       url='https://github.com/suciutwo/cocktails/',
-      packages=['src', 'src.data_processing', 'src.data_visualization'],
-      install_requires=['regex', 'enum34', 'numpy', 'pandas', 'scipy']
-      )
+      packages= ['src', 'src.data_processing', 'src.data_visualization'],
+      package_data = {'data':['/data/*']},
+      install_requires=['regex', 'enum34', 'numpy', 'pandas', 'scipy'],
+      include_package_date=True
+)
