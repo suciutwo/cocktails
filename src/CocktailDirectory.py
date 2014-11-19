d22fa9e621245386918109799cecda31bf3081d3
suciutwo <andrewsuciu@gmail.com>
2014-11-19 15:05:50 -0800

    Added interface class and tests.

---
+++ src/CocktailDirectory.py
@@ -0,0 +1,86 @@
+"""A directory of cocktails for searching."""
+import random
+from data_processing.matrix_generation import recipe_data
+
+
+class CocktailDirectory:
+
+    _dictionary = {}
+    _ingredients = set([])
+
+    def __init__(self):
+        """Parse cocktail recipes from disk if not parsed."""
+        if self._dictionary:
+            return
+        dataframe = recipe_data(None)
+        for name, ingredient_series in dataframe.iterrows():
+            recipe = []
+            ingredient_series.sort()
+            for ingredient, amount in ingredient_series.iteritems():
+                if amount > 0:
+                    ingredient = ingredient.replace('_', ' ')
+                    self._ingredients.add(ingredient)
+                    recipe.append((ingredient, amount))
+            self._dictionary[name] = recipe
+
+    def random_drinks(self, n):
+        """ Select n random elements from the directory.
+            Return [(key, value)]."""
+        selection = random.sample(self._dictionary.items(), n)
+        return [(name, recipe) for name, recipe in selection]
+
+    def all_ingredients(self):
+        """ Provide a list of all ingredient names. """
+        return list(self._ingredients)
+
+    def recipe(self, name):
+        """Provide the recipe for a single name."""
+        return self._dictionary.get(name)
+
+    def search(self, required=None, forbidden=None):
+        """All cocktails without forbidden elements, but with required."""
+        if required:
+            required = {i for i in required if i in self._ingredients}
+        if forbidden:
+            forbidden = {i for i in forbidden if i in self._ingredients}
+        if not required and not forbidden:
+            return self._dictionary.items()
+        allowed = []
+        for name, recipe in self._dictionary.iteritems():
+            ingredients = {ingredient for ingredient, amount in recipe}
+            if required and not required.intersection(ingredients):
+                continue
+            if forbidden and forbidden.intersection(ingredients):
+                continue
+            allowed.append((name, recipe))
+        return allowed
+
+    def flexible_search(self, liquor_on_shelf, allowed_missing_elements=0):
+        """Return all cocktails that can be made, buying only n ingredients."""
+        if liquor_on_shelf:
+            liquor_on_shelf = {i for i in liquor_on_shelf if i in self._ingredients}
+        else:
+            liquor_on_shelf = set([])
+        allowed = {}
+        for name, recipe in self._dictionary.iteritems():
+            ingredients = {ingredient for ingredient, amount in recipe}
+            missing = ingredients - liquor_on_shelf
+            if len(missing) == allowed_missing_elements:
+                missing = list(missing)
+                missing.sort()
+                key = '*^*'.join(missing)
+                if key not in allowed:
+                    allowed[key] = set([])
+                allowed[key].add(name)
+
+        sorted_keys = sorted(allowed, reverse=True, key=lambda k: len(allowed[k]))
+        result = []
+        for key in sorted_keys:
+            ingredients = key.split('*^*')
+            result.append((ingredients, allowed[key]))
+        return result
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ tests/test_CocktailDirectory.py
@@ -0,0 +1,59 @@
+"""Test for CocktailDirectory"""
+
+from src.CocktailDirectory import CocktailDirectory
+
+
+class TestCocktailDirectory:
+
+    def __init__(self):
+        self.dir = None
+
+    def setup(self):
+        self.dir = CocktailDirectory()
+
+    def test_recipe(self):
+        assert self.dir.recipe('martini') is None
+        martini_recipe = [(u'dry vermouth', 0.25), (u'lemon twist', 0.5), (u'olive', 0.5), (u'gin', 2.25)]
+        martini_result = self.dir.recipe('Martini (extra Dry)')
+        assert set(martini_result) == set(martini_recipe), martini_result
+
+    def test_random_drinks(self):
+        assert len(self.dir.random_drinks(4)) > 3
+        assert len(self.dir.random_drinks(5)) == 5
+
+    def test_all_ingredients(self):
+        count = len(self.dir.all_ingredients())
+        assert count == 450, "ALL INGREDIENTS " + str(count)
+
+    def test_search_all(self):
+        d = self.dir
+        all_recipes = d.search()
+        assert len(all_recipes) == 4502, len(all_recipes)
+
+    def test_search_gin(self):
+        d = self.dir
+        drinks_with_gin = d.search(required=['gin'])
+        assert len(drinks_with_gin) == 1275, len(drinks_with_gin)
+        drinks_with_gin = d.search(required=['gin', 'gklkakja'])
+        assert len(drinks_with_gin) == 1275, len(drinks_with_gin)
+
+    def test_search_no_gin(self):
+        d = self.dir
+        drinks_no_gin = d.search(forbidden=['gin'])
+        assert len(drinks_no_gin) == 3227, len(drinks_no_gin)
+        drinks_no_gin = d.search(required=['kkkkjdk'], forbidden=['gin', 'something madeup??!'])
+        assert len(drinks_no_gin) == 3227, len(drinks_no_gin)
+
+    def test_search_no_gin_with_vodka(self):
+        d = self.dir
+        drinks_with_gin = d.search(required=['vodka'], forbidden=['gin'])
+        assert len(drinks_with_gin) == 208, len(drinks_with_gin)
+
+    def test_flex_search(self):
+        d = self.dir
+        can_make = d.flexible_search(['gin'], 1)
+        # for thing in can_make:
+        #     print thing
+        assert len(can_make) == 33, len(can_make)
+        assert set(can_make[0][0]) == {u'sweet vermouth'}, can_make[0][0]
+        assert len(can_make[0][1]) == 4, can_make[0][1]
