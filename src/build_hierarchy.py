d88858d3d49d20e92e0b22e44017bd1f52cb714b
suciutwo <andrewsuciu@gmail.com>
2015-02-07 19:05:19 -0800

    Hierarchy handles empty answers better and lumps identical answers.

--- src/build_hierarchy.py
+++ src/build_hierarchy.py
@@ -2,6 +2,7 @@
 Constructs a tree that classifies drinks, saves it as JSON
 """
 
+from scipy.spatial.distance import cosine
 
 import src.data_processing.matrix_generation as generator
 from src.data_processing.matrix_generation import Normalization
@@ -13,6 +14,7 @@ class HierarchyNode:
     """
     Building blocks of the tree.
     """
+
     def __init__(self):
         self.question = None
         self.title = None
@@ -22,6 +24,8 @@ class HierarchyNode:
 
 def is_too_small(matrix):
     return len(matrix) < 20
+
+
 # to be reconsidered
 
 
@@ -35,53 +39,64 @@ def get_all_drinks(frame):
 
 def split_matrix_into_nodes(frame, depth, parent_answers):
     print "NEW CALL TO SPLIT", frame.shape
-    if is_too_small(frame) or depth > 1:
+    if is_too_small(frame) or depth > 2:
         return None
-    
+
     root = HierarchyNode()
     n_factors = 5
+    factors = None
     while True:
         factors = nmf_factors(frame, n_factors)
         print_top_components(factors, frame.columns, {})
-        root.question = raw_input("Overall name for question? (" + ' '.join(parent_answers) + ") ")
+        root.question = raw_input("Overall name for question? (%d drinks here) (%s)) " 
+                                  % (len(frame), ' '.join(parent_answers)))
+        if not root.question:
+            return None
         if root.question.isdigit():
             n_factors = int(root.question)
             print "RERUNNING WITH", n_factors, "FACTORS INSTEAD"
         else:
             break
-            
 
-    final_factors = []
+    final_factors = {}
     for index, factor in enumerate(factors):
         print_component(factor, frame.columns, {}, index)
-        factor_label = raw_input("Name for factor " + str(index+1) + ": ")
+        factor_label = raw_input("Name for factor %d: " % (index + 1))
         if factor_label:
-            print "factor", index+1, "named", factor_label
-            factor_data = {'label': factor_label, 
-                           'factor': factor, 
-                           'indices': []}
-            final_factors.append(factor_data)
-    
+            print "factor", index + 1, "named", factor_label
+            if factor_label in final_factors:
+                final_factors[factor_label]['factors'].append(factor)
+            else:
+                final_factors[factor_label] = {
+                    'factors': [factor],
+                    'indices': []}
+    if not final_factors:
+        return None
+
     for row_number in range(frame.shape[0]):
         row = frame.ix[row_number, :]
         print row.name
-        maximum_product = 0
-        maximum_label = None
-        for data in final_factors:
-            factor = data['factor']
-            result = row.dot(factor)
-            print result
-            if result > maximum_product:
-                maximum_product = result
-                maximum_label = data['label']
-        for data in final_factors:
-            if maximum_label == data['label']:
+        minimum_distance = None
+        minimum_label = None
+        for label, data in final_factors.iteritems():
+            factors = data['factors']
+            for factor in factors:
+                distance = cosine(row, factor)
+                print distance
+                if not minimum_distance or distance < minimum_distance:
+                    minimum_distance = distance
+                    minimum_label = label
+        for label, data in final_factors.iteritems():
+            if minimum_label == label:
                 data['indices'].append(row_number)
                 break
 
-    for data in final_factors:
+    final_factors = {label: data for label, data in final_factors.iteritems()
+                     if data['indices']}
+
+    for label, data in final_factors.iteritems():
         child = HierarchyNode()
-        child.title = data['label']
+        child.title = label
         relevant_rows = frame.ix[data['indices'], :]
         print "RECURSIVE CALL FOR", relevant_rows.shape
         current_answers = list(parent_answers)
@@ -93,26 +108,29 @@ def split_matrix_into_nodes(frame, depth, parent_answers):
         else:
             child.drinks = get_all_drinks(relevant_rows)
         root.children.append(child)
-    
+
     return root
-        
+
 
 def run():
     # Safely load tree from disk (or recreate)
     import pickle
+
     try:
         tree = pickle.load(open('tree.pickle', 'rb'))
     except:
         matrix = generator.recipe_data(Normalization.EXACT_AMOUNTS)
         tree = split_matrix_into_nodes(matrix, 0, [])
         pickle.dump(tree, open('tree.pickle', 'wb'))
-    
+
     # Save it as JSON
     import jsonpickle
+
     jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=2)
     f = open('tree.json', 'wb')
     f.write(jsonpickle.encode(tree))
-    
+
+
 if __name__ == '__main__':
     run()
 
